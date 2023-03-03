c457bc1bfc2a015d889ed591f3dcfcbb4e9ce11c
Minhui-Chen <minhui.chen.8907@hotmail.com>
2023-03-02 20:32:56 -0600

    update

---
+++ bin/sim/he.py
@@ -0,0 +1,45 @@
+import re, os, sys
+import numpy as np, pandas as pd
+
+from gxctmm import log, fit
+
+def main():
+
+    batch = snakemake.params.batches
+    outs = [re.sub('/rep/', f'/rep{i}/', snakemake.params.out) for i in batch]
+    for Y_f, K_f, ctnu_f, P_f, out_f in zip(
+            [line.strip() for line in open(snakemake.input.cty)],
+            [line.strip() for line in open(snakemake.input.K)],
+            [line.strip() for line in open(snakemake.input.ctnu)], 
+            [line.strip() for line in open(snakemake.input.P)],
+            outs):
+
+        log.logger.info(f'{Y_f}\n{K_f}\n{ctnu_f}\n{P_f}\n')
+
+        os.makedirs(os.path.dirname(out_f), exist_ok=True)
+        
+        # read
+        Y = np.loadtxt( Y_f )
+        K = np.loadtxt( K_f )
+        ctnu = np.loadtxt( ctnu_f )
+        P = np.loadtxt( P_f )
+
+        ## Free
+        free_he, free_he_wald = fit.free_HE(Y, K, ctnu, P)
+
+        # Full
+        full_he = fit.full_HE(Y, K, ctnu, P)
+
+        # save
+        np.save(out_f,
+                {'free':free_he, 'full':full_he, 
+                'wald': {'free':free_he_wald} 
+                }
+            )
+        
+    with open(snakemake.output.out, 'w') as f:
+        f.write('\n'.join(outs))  
+
+
+if __name__ == '__main__':
+    main()
