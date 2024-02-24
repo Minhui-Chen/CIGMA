7bc515f5fe5e06e757d66882437684f82f132f33
Minhui-Chen <minhui.chen.8907@hotmail.com>
2024-02-23 20:40:00 -0600

    removed files

--- bin/yazar/impute.R
+++
@@ -1,7 +0,0 @@
-source("bin/yazar/softImpute.R")
-
-data <- read.table(snakemake@input[["data"]], sep='\t', header=F)
-mat <- as.matrix(data)
-
-out <- my_softImpute(mat, scale=T, seed=as.integer(snakemake@params[["seed"]]))
-write.table(out$Y, snakemake@output[["data"]], sep='\t', quote=F, row.names=F, col.names=F)
